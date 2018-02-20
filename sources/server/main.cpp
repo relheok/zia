36c7e3625362a63e78d3fe963d75bcdbd9c8d8c9
koehle_j <jeremy.koehler@epitech.eu>
2018-02-20 21:19:37 +0100

    Accept options in main

--- sources/server/main.cpp
+++ sources/server/main.cpp
@@ -5,81 +5,89 @@
 // Login   <albert_q@epitech.net>
 //
 // Started on  Sun Nov  5 16:42:43 2017 Quentin Albertone
-// Last update Tue Feb 20 16:15:38 2018 Jérémy Koehler
+// Last update Tue Feb 20 21:18:25 2018 Jérémy Koehler
 //
 
 #include "main.hpp"
 
-/* BEGIN TEST CODE */
-int fd_cliGlobal;
-int fd_inetGlobal;
 
+#include <getopt.h>
+#include "daemon.hpp"
+#include "http/HttpInterpreter.hpp"
+#include "module_manager/ModuleManager.hpp"
 
-# include <sys/un.h>
-# include <sys/socket.h>
+int		process(std::string confPath)
+{
+  try {
+    std::cout << "Run with config file: " << confPath << std::endl;
+    zia::api::ConfigManager p(confPath);
+    zia::Daemon &daemon = zia::Daemon::getInstance();
 
-# include <unistd.h>
-# include <stdio.h>
+    daemon.setConf(&p);
 
-# include <string.h>
+    while (daemon.isAlive()) {
+      /*
+      ** TODO
+      */
+    }
+  } catch (std::exception &e) {
+    std::cerr << e.what() << std::endl;
+    return (1);
+  }
+  return (0);
+}
 
-# include <netinet/in.h>
+int		main(int argc, char **argv)
+{
+  static struct option long_options[] =
+    {
+      {"config", required_argument, 0, 'c'},
+      {"reload", no_argument, 0, 'r'},
+      {"signal", required_argument, 0, 's'}
+    };
+  int option_index = 0;
+  int c;
+  std::string confPath("yolo.json"); // this will be the default value of our conf path
 
-# include "http/HttpInterpreter.hpp"
-# include "module_manager/ModuleManager.hpp"
+  while ((c = getopt_long (argc, argv, "c:rs:",
+			   long_options, &option_index)) != -1)
+    {
+      switch (c)
+	{
+	case 'c':
+	  confPath = optarg;
+	  break;
+	case 'r':
+	  zia::Daemon::sendSignal("reload");
+	  return (0);
+	case 's':
+	  zia::Daemon::sendSignal(optarg);
+	  return (0);
+	}
+    }
+
+  return (process(confPath));
+
+  // backlog
+  /* Print any remaining command line arguments (not options). */
+  if (optind < argc)
+    {
+      printf ("non-option ARGV-elements: ");
+      while (optind < argc)
+        printf ("%s ", argv[optind++]);
+      putchar ('\n');
+    }
+
+  return (0);
+}
 
 # define BUFFSIZE 4096
 # define _DEBUG_PORT 4243
 
-typedef struct sockaddr_in	t_sockaddr_in;
-typedef	struct sockaddr_un	t_sockaddr_un;
-typedef struct sockaddr		t_sockaddr;
+int		create_socket(int port);
+int		accept_socket(int fd, int port);
 
-// Inet Socket part
-int			create_socket(int port)
-{
-  int			fd;
-  t_sockaddr_in		addr;
-
-  if ((fd = socket(AF_INET, SOCK_STREAM, 0)) < 3)
-    {
-      printf("Error socket: %d\n", fd);
-      return (fd);
-    }
-  addr.sin_family = AF_INET;
-  addr.sin_addr.s_addr = INADDR_ANY;
-  addr.sin_port = htons(port);
-  if (bind(fd, (t_sockaddr *)&addr, sizeof(t_sockaddr_in)) < 0)
-    {
-      printf("Error bind\n");
-      return (0);
-    }
-  if (listen(fd, 42))
-    {
-      printf("Error listen\n");
-      return (-10);
-    }
-  return (fd);
-}
-
-int			accept_socket(int fd, int port)
-{
-  t_sockaddr_in		addr;
-  int			fd_cli;
-  int			addrlen;
-
-  addrlen = sizeof(addr);
-  addr.sin_family = AF_INET;
-  addr.sin_addr.s_addr = INADDR_ANY;
-  addr.sin_port = htons(port);
-  if ((fd_cli = accept(fd, (t_sockaddr *)&addr, (socklen_t *)&addrlen)) == -1)
-    printf("Error accept: %d\n", fd_cli);
-  printf("New client fd: %d\n", fd_cli);
-  return (fd_cli);
-}
-/* END TEST CODE */
-
-int		main(int ac, char **av)
+int		old_main(int ac, char **av)
 {
   try {
     std::string path((ac == 2) ? (av[1]) : (""));
@@ -88,6 +96,8 @@ int		main(int ac, char **av)
       //zia::Daemon &daemon = zia::Daemon::getInstance();
 
       /* BEGIN TEST CODE */
+      int			fd_cliGlobal;
+      int			fd_inetGlobal;
       char			buff[BUFFSIZE];
       zia::api::HttpInterpreter interpreter(std::map<std::string, std::string>{{"localhost:4243", "."}}, zia::api::ModulesList());
 
