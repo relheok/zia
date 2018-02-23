a7126f25a987cbee1f2753a738cb8612ef8b9654
Loris Sanchez <loris1.sanchez@epitech.eu>
2018-02-23 17:15:23 +0100

    remove useless lines

--- includes/modules/getpost.hpp
+++ includes/modules/getpost.hpp
@@ -1,11 +1,6 @@
 #ifndef __GETPOST_H__
 # define __GETPOST_H__
 
-#include <string>
-#include <stdlib.h>
-#include <map>
-#include <new>
-
 std::string	urlDecode(std::string str);
 void		initializeGet(std::map <std::string, std::string> &Get);
 void		initializePost(std::map <std::string, std::string> &Post);
