e4a63ea9a4d3a5538ab5beceb5ae86247b6d2963
Loris Sanchez <loris1.sanchez@epitech.eu>
2018-02-23 17:38:51 +0100

    fix

--- includes/modules/getpost.hpp
+++ includes/modules/getpost.hpp
@@ -1,6 +1,12 @@
 #ifndef __GETPOST_H__
 # define __GETPOST_H__
 
+#include <iostream>
+#include <map>
+#include <new>
+#include <unistd.h>
+#include <cstring>
+
 std::string	urlDecode(std::string str);
 void		initializeGet(std::map <std::string, std::string> &Get);
 void		initializePost(std::map <std::string, std::string> &Post);
