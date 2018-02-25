740a930b31680c928c147726d907c91ee4bf64e1
Rodolphe Chazal <rodolphe.chazal@epitech.eu>
2018-02-25 20:28:49 +0100

    Makefile for unitTest

---
+++ linux/includes/unitTest/unitTest.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "catch.hpp"
+#include "SSLModule.hpp"
+#include "config_manager/ConfigManager.hpp"
+#include "config_manager/json-lib/JSON.h"
+#include "config_manager/json-lib/JSONValue.h"
--- linux/sources/unitTest/unitTest.cpp
+++ linux/sources/unitTest/unitTest.cpp
@@ -1,41 +1,12 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
-#include "catch.hpp"
+#include "unitTest/catch.hpp"
 
 unsigned int Factorial( unsigned int number ) {
     return number <= 1 ? number : Factorial(number-1)*number;
 }
 
-TEST_CASE("vectors can be sized and resized", "[vector]") {
+TEST_CASE("Openssl certificate test", "[Openssl]") {
 
-    std::vector<int> v( 5 );
-
-    REQUIRE( v.size() == 5 );
-    REQUIRE( v.capacity() >= 5 );
-
-    SECTION( "resizing bigger changes size and capacity" ) {
-        v.resize( 10 );
-
-        REQUIRE( v.size() == 10 );
-        REQUIRE( v.capacity() >= 10 );
-    }
-    SECTION( "resizing smaller changes size but not capacity" ) {
-        v.resize( 0 );
-
-        REQUIRE( v.size() == 0 );
-        REQUIRE( v.capacity() >= 5 );
-    }
-    SECTION( "reserving bigger changes capacity but not size" ) {
-        v.reserve( 10 );
-
-        REQUIRE( v.size() == 5 );
-        REQUIRE( v.capacity() >= 10 );
-    }
-    SECTION( "reserving smaller does not change size or capacity" ) {
-        v.reserve( 0 );
-
-        REQUIRE( v.size() == 5 );
-        REQUIRE( v.capacity() >= 5 );
-    }
 }
 
 TEST_CASE( "Factorials are computed", "[factorial]" ) {
